54a22246e33f6d6c10d516f42cca3ea8e9e4294b
jkrperson <josekristianresabal@gmail.com>
2024-03-18 14:57:22 +0800

    Add test_model.py for classification model evaluation

---
+++ test_model.py
@@ -0,0 +1,22 @@
+from models.classification_model import ClassifierModel
+from data.verafiles import VeraFilesDataset
+from transformers import AutoTokenizer
+import torch.nn.functional as F
+
+data = VeraFilesDataset("datasets/verafiles_dataset/test.csv", None)
+model = ClassifierModel.load_from_checkpoint("checkpoint/best-checkpoint-v1.ckpt")
+tokenizer = AutoTokenizer.from_pretrained("google-bert/bert-base-multilingual-cased")
+
+text, label = data[13]
+
+# 401 is correct
+
+tokenized = tokenizer(text, padding=True, max_length=512, truncation=True, return_tensors="pt")
+model.freeze()
+preds = model(tokenized["input_ids"], tokenized["attention_mask"])
+preds = F.softmax(preds, dim=1)
+
+print(f"OUTPUTS: {preds}")
+idx = preds.argmax(1)
+print(f"TEXT: {text}")
+print(f"ACTUAL LABEL: {data.id2label[label]} \t PREDICTED LABEL: {data.id2label[idx.item()]}")
