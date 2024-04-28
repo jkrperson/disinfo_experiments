cb002204739ce29be1d36947581ae671294583d3
jkrperson <josekristianresabal@gmail.com>
2024-04-28 20:01:43 +0800

    Add fakenewstl_tlelectra.sh script for training with Electra model on fakenewsTL dataset

---
+++ fakenewstl_tlelectra.sh
@@ -0,0 +1,1 @@
+python classification_trainer.py --model_name="jcblaise/electra-tagalog-base-cased-discriminator" --dataset_name="fakenewsTL" --experiment_name="electra_tagalog_fakenewstl" --max_epochs=60 --learning_rate=1e-5
