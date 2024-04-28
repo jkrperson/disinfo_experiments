1e36e3d822d84785cd29f2f2ccb0cbbf64914ea6
jkrperson <josekristianresabal@gmail.com>
2024-04-28 10:51:05 +0800

    create trainer script for fakenewsTL with mBERT

---
+++ fakenewstl_mbert.sh
@@ -0,0 +1,1 @@
+python classification_trainer.py --model_name="google-bert/bert-base-multilingual-cased" --dataset_name="fakenewsTL" --experiment_name="mBERT_fakenewsTL" --max_epochs=30 --learning_rate=2e-5
