2a44a9ce12d6f27d46cdd6f89f3d3be99b523268
jkrperson <josekristianresabal@gmail.com>
2024-04-29 10:01:36 +0800

    Add classification script for XLM-RoBERTa model on verafiles_balanced dataset

---
+++ running_scripts/classify_tlroberta_balanced.sh
@@ -0,0 +1,1 @@
+python classification_trainer.py --model_name="jcblaise/roberta-tagalog-base" --dataset_name="verafiles_balanced" --experiment_name="robertatl_verafiles_balanced" --max_epochs=60 --learning_rate=5e-6
