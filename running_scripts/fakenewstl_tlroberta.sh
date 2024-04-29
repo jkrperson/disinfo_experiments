49ca64482d3c1fd889a9c479204fe3384398da50
jkrperson <josekristianresabal@gmail.com>
2024-04-29 10:35:32 +0800

    Add classification script for XLM-RoBERTa model on fakenewsTL dataset

---
+++ running_scripts/fakenewstl_tlroberta.sh
@@ -0,0 +1,1 @@
+python classification_trainer.py --model_name="jcblaise/roberta-tagalog-base" --dataset_name="fakenewsTL" --experiment_name="electra_tagalog_fakenewstl" --max_epochs=60 --learning_rate=1e-5
