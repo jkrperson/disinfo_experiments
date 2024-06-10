8cb34e3393a40d50ee1a11cc3ebc202f506d1f31
jkrperson <josekristianresabal@gmail.com>
2024-06-10 23:42:44 +0800

    refactor: Update script names and model names for running classification on fakenewsTL dataset

---
+++ running_scripts/classify_electratl_fakenewstl.sh
@@ -0,0 +1,1 @@
+python classification_trainer.py --model_name="jcblaise/electra-tagalog-base-cased-discriminator" --dataset_name="fakenewsTL" --experiment_name="electratl_fakenewsTL" --max_epochs=60 --learning_rate=1e-5
